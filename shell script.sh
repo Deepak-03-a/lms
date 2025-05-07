e52a79a8dac40be0ce3b61c00be20f41a9fd9d97
Deepak-03-a <kasavajahalaramdeepak@gmail.com>
2025-05-07 10:12:39 +0530

    created a shell script.sh which will install jenkins, docker, minikube and kubectl inside the server

---
+++ shell script.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+# for updating the system
+sudo apt update
+
+# to install jenkins inside the server
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+
+# to install docker inside the server
+wget -O get-docker.sh https://get.docker.com && sh get-docker.sh
+sudo usermod -aG docker ubuntu
+
+# To install minikube inside the server
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+# commands to install kubectl
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/kubectl
